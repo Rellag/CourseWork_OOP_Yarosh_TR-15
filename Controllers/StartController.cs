999926724394514df3e83b403b9c7d1e204ba19d
Rellag <62191338+Rellag@users.noreply.github.com>
2023-01-09 21:51:55 +0200

    WithList

--- Controllers/StartController.cs
+++ Controllers/StartController.cs
@@ -3,52 +3,49 @@ namespace CourseWork.Controllers
 {
     public class StartController
     {
+        List<Customer> customers = new List<Customer>();
+        List<IRun> runs = new List<IRun>();
+
         public void Run()
         {
             Console.WriteLine("Інтернет магазин");
 
-            List<Customer> customers = new List<Customer>();
+            runs.Add(new BalanceController());
+            runs.Add(new HistoryController());
+            runs.Add(new GoToShopController());
+            runs.Add(new ShopCartController());
+
 
             Customer CurrCustomer;
             {
-                Console.Clear();
-                LogInController logIn = new LogInController();
-                CurrCustomer = logIn.Run(customers);
-
                 while (true)
                 {
+
                     Console.Clear();
+                    LogInController logIn = new LogInController();
+                    CurrCustomer = logIn.Run(customers);
 
-                    Console.WriteLine("Оберіть команду:");
-                    Console.WriteLine("1 - Поповнити баланс\n2 - Подивитись історію покупок\n3 - Перейти до магазину\n4 - Переглянути кошик\n0 - Назад");
-                    int typed = Convert.ToInt32(Console.ReadLine());
-                    Console.Clear();
+                    while (true)
+                    {
+                        Console.Clear();
 
-                    if (typed == 0)
-                    {
-                        break;
-                    }
-                    else if (typed == 1)
-                    {
-                        BalanceController balance = new BalanceController();
-                        balance.Run(CurrCustomer);
-                    }
-                    else if (typed == 2)
-                    {
-                        HistoryController history = new HistoryController();
-                        history.Run(CurrCustomer);
-                    }
-                    else if (typed == 3)
-                    {
-                        GoToShopController shop = new GoToShopController();
-                        shop.Run(CurrCustomer);
-                    }
-                    else if (typed == 4)
-                    {
-                        ShopCartController cartController = new ShopCartController();
-                        cartController.Run(CurrCustomer);
-                    }
+                        Console.WriteLine("Оберіть команду:");
+                        Console.WriteLine("1 - Поповнити баланс\n2 - Подивитись історію покупок\n3 - Перейти до магазину\n4 - Переглянути кошик\n0 - Назад");
+                        int typed = Convert.ToInt32(Console.ReadLine());
+                        Console.Clear();
 
+                        if (typed == 0)
+                        {
+                            break;
+                        }
+                        else if (typed > 0 && typed < runs.Count())
+                        {
+                            typed--;
+                            runs[typed].Run(CurrCustomer);
+                        }
+
+
+                    }
                 }
 
 
